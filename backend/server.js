7e42a443ca98f63e19c6058a483b42c32c14a59f
Kevin Trinh <kevintr1005@gmail.com>
2026-03-27 18:35:48 -0700

    Redesigned the entire U
    I didn't like the original design, so I started it all over and come up with this

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = 4000;
-
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-app.listen(PORT);
